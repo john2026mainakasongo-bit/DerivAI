b9b06fba4e95d2f400f16d2b33226ef98e96fcdb
john maina <YOUR_GITHUB_EMAIL>
2026-08-01 16:48:04 +0300

    Remove invalid OAuth client ID from public socket

--- src/services/derivApi.js
+++ src/services/derivApi.js
@@ -1,6 +1,5 @@
 ﻿const PUBLIC_SOCKET_URLS = [
-  "wss://ws.binaryws.com/websockets/v3?app_id=33ZwwSOS2hgdlG91Hbk8Q&l=EN&brand=deriv",
-  "wss://ws.binaryws.com/websockets/v3?app_id=33ZwwSOS2hgdlG91Hbk8Q",
+  "wss://ws.binaryws.com/websockets/v3",
 ];
 
 const API_BASE_URL = "https://api.derivws.com";
