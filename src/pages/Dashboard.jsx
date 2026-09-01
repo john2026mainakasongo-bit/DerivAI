920a9113755ab8f1d9972b64e760e733b6305b0a
john maina <YOUR_GITHUB_EMAIL>
2026-09-01 12:04:41 +0300

    Make Rise Fall Touch tool the main dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,5 @@
-﻿import "../styles/StrategyEngineV36.css";
-import StrategyEngineV36 from "../components/StrategyEngineV36";
-import RiseFallTouchAnalysis from "./RiseFallTouchAnalysis";
+﻿import RiseFallTouchAnalysis from "./RiseFallTouchAnalysis";
 
 export default function Dashboard() {
-  return (
-    <>
-      <StrategyEngineV36 />
-      <RiseFallTouchAnalysis />
-    </>
-  );
+  return <RiseFallTouchAnalysis />;
 }
