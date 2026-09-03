e39f951323f054b5cc6f1f40f459ab7dadbb8aab
john maina <YOUR_GITHUB_EMAIL>
2026-09-03 11:23:40 +0300

    Reset project to clean Deriv foundation

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-﻿import { DerivAuthProvider } from "./auth/DerivAuthContext";
-import "./styles/DerivOAuth.css";
-import "./styles/LaptopFullWidthOneScreen.css";
-import "./styles/LaptopFullPage.css";
-import React from "react";
+﻿import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import { DerivAuthProvider } from "./auth/DerivAuthContext";
+import App from "./App";
+
+import "./index.css";
+import "./clean.css";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
-    <DerivAuthProvider><App /></DerivAuthProvider>
+    <DerivAuthProvider>
+      <App />
+    </DerivAuthProvider>
   </React.StrictMode>
 );
-
-
